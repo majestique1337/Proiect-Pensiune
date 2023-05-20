b8f5b1f4d660cc66c694094c7b16058789420902
Robert <101188280+majestique1337@users.noreply.github.com>
2023-05-21 00:59:51 +0300

    90% done
    almost all ready, emailjs is working fine.

--- scripts.js
+++ scripts.js
@@ -24,3 +24,62 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+
+ // Initialize EmailJS
+emailjs.init('7AzvBrkzNk0QkifD3');
+
+// Initialize Flatpickr
+flatpickr("#start", {
+    dateFormat: "d/m/Y",
+    minDate: "today",
+    onChange: function(selectedDates, dateStr, instance) {
+        endDatePicker.set("minDate", dateStr);
+    }
+});
+
+var endDatePicker = flatpickr("#end", {
+    dateFormat: "d/m/Y",
+    minDate: "today"
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const telefon = document.getElementById('telefon').value;
+    const start = document.getElementById('start').value;
+    const end = document.getElementById('end').value;
+    const message = document.getElementById('message').value;
+
+    // Form validation can go here
+
+    emailjs.send('service_vx5gczg', 'template_bt9xaum', {
+      to_name: 'Casa Alunis', // Replace with the recipient name or team name
+      reply_to: email,
+      name: name,
+      telefon: telefon,
+      start_date: start,
+      end_date: end,
+      message: message
+    })
+    .then(function(response) {
+      console.log('Success!', response.status, response.text);
+
+      // Show success message
+      document.getElementById('success-message').style.display = "block";
+      document.getElementById('success-message').innerText = "Thank you! Your message has been successfully sent. We will contact you very soon!";
+
+      // Reset form
+      document.getElementById('contact-form').reset();
+  }, function(error) {
+      console.log('Failed...', error);
+  });
+});
+
+
+
+
+
+
+
